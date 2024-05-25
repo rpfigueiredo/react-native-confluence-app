dbd9276cf8b7f4823a292004f6d37a5d02427749
rpfigueiredo <rodrigo.prado.figueiredo@gmail.com>
2024-05-25 17:05:01 -0300

    Corrige bugs e implementa o perfil do usuario

--- confluence-mobile/app/(auth)/sign-in.jsx
+++ confluence-mobile/app/(auth)/sign-in.jsx
@@ -6,16 +6,16 @@ import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Link, router } from "expo-router";
-import { signIn } from "../../lib/appwrite";
+import { getCurrentUser, signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignIn = () => {
-
+  const { setUser, setIsLogged } = useGlobalContext();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const submit = async () => {
     if (form.email === "" || form.password === "") {
@@ -29,8 +29,6 @@ const SignIn = () => {
       const result = await getCurrentUser();
       setUser(result);
       setIsLogged(true);
-
-      Alert.alert("Success", "Usuário conectado com sucesso");
       router.replace("/home");
     } catch (error) {
       Alert.alert("Error", error.message);
--- confluence-mobile/app/(tabs)/home.jsx
+++ confluence-mobile/app/(tabs)/home.jsx
@@ -10,9 +10,11 @@ import EmptyState from '../../components/EmptyState';
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard';
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
 
+  const { user } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts} = useAppwrite(getLatestPosts);
 
@@ -39,10 +41,10 @@ const Home = () => {
             <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Bem-vindo de volta
+                  Bem-vindo de volta,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Rodrigo
+                {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
