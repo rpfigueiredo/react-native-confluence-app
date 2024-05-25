a3f203f4fa4d245d88b0c76892b60bb8b13d83db
rpfigueiredo <rodrigo.prado.figueiredo@gmail.com>
2024-05-25 01:53:32 -0300

    Implementado tela de pesquisa

--- confluence-mobile/app/search/[query].jsx
+++ confluence-mobile/app/search/[query].jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { FlatList, Text, View} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInput from '../../components/SearchInput';
+import EmptyState from '../../components/EmptyState';
+import { searchPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '../../components/VideoCard';
+import { useLocalSearchParams } from 'expo-router';
 
 const Search = () => {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+
+  const { query } = useLocalSearchParams()
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch()
+  }, [query])
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Resultado da pesquisa
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">
+              {query}
+            </Text>
+            <View className="mt-6 mb-8">
+                <SearchInput initialQuery={query} refetch={refetch} />
+              </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="Nenhum vídeo encontrado"
+            subtitle="Nenhum vídeo encontrado para esta pesquisa"
+          />
+        )}
+
+      />
+    </SafeAreaView>
+  )
+}
 
 export default Search
--- confluence-mobile/components/SearchInput.jsx
+++ confluence-mobile/components/SearchInput.jsx
@@ -1,29 +1,42 @@
 import React from 'react'
-import { Text, View, TextInput, TouchableOpacity, Image  } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
 import { useState } from "react";
 
 import { icons } from "../constants";
+import { router, usePathname } from 'expo-router';
 
-const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
+const SearchInput = ({initialQuery}) => {
 
-    const [showPassword, setShowPassword] = useState(false);
+    const pathname = usePathname()
+    const [query, setQuery] = useState(initialQuery || '')
 
     return (
-            <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary flex-row items-center space-x-4">
-                <TextInput
-                    className="text-base mt-0.5 text-white flex-1 font-pregular"
-                    value={value}
-                    placeholder="Pesquisar um tópico de vídeo"
-                    placeholderTextColor="#7B7B8B"
-                    onChangeText={handleChangeText}
-                />
-                <TouchableOpacity>
-                    <Image 
-                    source={icons.search} 
-                    className="w-5 h-5" 
+        <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary flex-row items-center space-x-4">
+            <TextInput
+                className="text-base mt-0.5 text-white flex-1 font-pregular"
+                value={query}
+                placeholder="Pesquisar um tópico de vídeo"
+                placeholderTextColor="#CDCDE0"
+                onChangeText={(e) => setQuery(e)}
+            />
+            <TouchableOpacity
+                onPress={() => {
+                    if (!query) {
+                        return Alert.alert(
+                            "Consulta ausente",
+                            "Por favor, insira algo para pesquisar "
+                        );
+                    }
+                    if (pathname.startsWith("/search")) router.setParams({ query });
+                    else router.push(`/search/${query}`);
+                }}
+            >
+                <Image
+                    source={icons.search}
+                    className="w-5 h-5"
                     resizeMode="contain" />
-                </TouchableOpacity>
-            </View>
+            </TouchableOpacity>
+        </View>
     )
 }
 
